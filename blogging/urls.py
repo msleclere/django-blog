277f1b31327af0e4cbf97ff5071653183d6b715c
Michelle Leclere <michelle.leclere98@gmail.com>
2024-09-03 20:13:36 -0700

    Added detail view, but posts not showing up at ''

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blogging.views import list_view
+from blogging.views import list_view, detail_view
 
 urlpatterns = [
-    path('posts/<int:post_id>/', list_view, name="blog_detail"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('', list_view, name="blog_index"),
 ]
