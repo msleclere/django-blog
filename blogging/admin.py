4471a362ee217eb6b6b133a5a647b6487f0a04d2
Michelle Leclere <michelle.leclere98@gmail.com>
2024-09-09 16:16:34 -0700

    Attempting admin page changes

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.db import models
 from blogging.models import Post, Category
 
-admin.site.register(Post)
 admin.site.register(Category)
+
+
+# admin.site.register(Post)
+
+class CategoryInLine(admin.StackedInline):
+    model = Category
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInLine, ]
+
+
+admin.site.register(Post, PostAdmin)
--- blogging/models.py
+++ blogging/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #category = models.ForeignKey(Category)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    #category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -15,7 +18,8 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    #posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
