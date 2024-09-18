83cf26c4b8a510c76eb80f210d7688ea15535a1b
Michelle Leclere <michelle.leclere98@gmail.com>
2024-09-17 19:00:57 -0700

    Updated detail view with class based in polling

--- polling/views.py
+++ polling/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 class PollListView(ListView):
@@ -9,18 +10,17 @@ class PollListView(ListView):
     template_name = 'polling/lists.html'
 
 
-def detail_view(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polling/detail.html'
 
-    if request.method == "POST":
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
         if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
-
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+        context = {'object': poll}
+        return render(request, 'polling/detail.html', context)
